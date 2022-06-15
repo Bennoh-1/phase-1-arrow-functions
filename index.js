3572e24f9ca7da02ff81b25d4f3ec44b7087f1f1
Bennoh-1 <franciskibira017@gmail.com>
2022-06-15 13:02:26 +0300

    new message

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-let divide = () => 2000/100
-let square = x => x * x;
-let add = (a, b) => a + b;
+const divide = function() {
+    return 2000 / 100;
+}
+
+const square = number => (number ** 2);
+
+const add = (x, y) => (x + y);
